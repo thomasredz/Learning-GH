9a0213ba8e56935de60894e852e391d52785b5e2
thomasredz <thomas.rosso65@gmail.com>
2022-09-05 08:52:10 +0200

    Initial Commit

---
+++ LearnngGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnngGH
+//
+//  Created by Thomas Rosso on 05/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnngGH/LearnngGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnngGHApp.swift
+//  LearnngGH
+//
+//  Created by Thomas Rosso on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnngGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
